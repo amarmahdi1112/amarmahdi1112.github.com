f09df8fc31e7dd7164d3b575059e0d3996d0c76e
Amaramhdi1112 <amarmahdi1112@gmail.com>
2023-05-23 14:22:41 -0600

    Resolve eslint linter issue

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,13 +4,13 @@ const nav = document.getElementById('show-nav');
 const overlay = document.getElementById('overlay');
 
 toggler.addEventListener('click', () => {
-    overlay.classList.remove('overlay-hide');
-    overlay.classList.add('overlay-show');
-    nav.classList.toggle('nav-show');
+  overlay.classList.remove('overlay-hide');
+  overlay.classList.add('overlay-show');
+  nav.classList.toggle('nav-show');
 });
 
 closeNav.addEventListener('click', () => {
-    overlay.classList.add('overlay-hide');
-    overlay.classList.remove('overlay-show');
-    nav.classList.remove('nav-show');
+  overlay.classList.add('overlay-hide');
+  overlay.classList.remove('overlay-show');
+  nav.classList.remove('nav-show');
 });
