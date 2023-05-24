d7a137d29b12c3cc38a1d64473fc6858c9076d8e
Amaramhdi1112 <amarmahdi1112@gmail.com>
2023-05-23 19:03:48 -0600

    Add link redirect

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,32 @@
 const toggler = document.getElementById('toggler');
 const closeNav = document.getElementById('close');
+const links = document.getElementsByClassName('links')
 const nav = document.getElementById('show-nav');
 const overlay = document.getElementById('overlay');
 
 toggler.addEventListener('click', () => {
-  overlay.classList.remove('overlay-hide');
-  overlay.classList.add('overlay-show');
-  nav.classList.toggle('nav-show');
+  tgl(true)
 });
 
 closeNav.addEventListener('click', () => {
-  overlay.classList.add('overlay-hide');
-  overlay.classList.remove('overlay-show');
-  nav.classList.remove('nav-show');
+  tgl(false)
+  console.log('hello')
 });
+
+Array.from(links).forEach(element => {
+  element.addEventListener('click', () => {
+    tgl(false)
+  })
+});
+
+const tgl = (show) => {
+  if (show) {
+    overlay.classList.remove('overlay-hide');
+    overlay.classList.add('overlay-show');
+    nav.classList.toggle('nav-show');
+  } else {
+    overlay.classList.add('overlay-hide');
+    overlay.classList.remove('overlay-show');
+    nav.classList.remove('nav-show');
+  }
+}
