4589c4a69c085c4b0b0a475e0232ef221277191f
Amaramhdi1112 <amarmahdi1112@gmail.com>
2023-05-25 12:20:57 -0600

    Add js form validation

---
+++ app/scripts/formValidation.js
@@ -0,0 +1,30 @@
+const contact = document.querySelector('#contact');
+const fullnameErr = document.querySelector('#fullname-err');
+const emailErr = document.querySelector('#email-err');
+const msgErr = document.querySelector('#msg-err');
+
+contact.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const elements = e.target;
+	const fullname = elements[0].value;
+	const email = elements[1].value;
+	const message = elements[2].value;
+	const fullnameCount = fullname.split(' ').length;
+
+	if (fullnameCount < 2) {
+		fullnameErr.innerText = 'Please provide your full name!';
+	} else {
+		fullnameErr.innerText = '';
+	}
+	const lwEmail = email.toLowerCase();
+	if (email !== lwEmail) {
+		emailErr.innerText = 'Please provide your valid email address!';
+	} else {
+		emailErr.innerText = '';
+	}
+	if (message.trim().length === 0) {
+		msgErr.innerText = 'Please say something!';
+	} else {
+		msgErr.innerText = '';
+	}
+});
