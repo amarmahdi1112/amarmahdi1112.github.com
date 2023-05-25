4b682989c8c6eb28df471e977e733cdd64718863
Amaramhdi1112 <amarmahdi1112@gmail.com>
2023-05-25 13:46:00 -0600

    Submit functionality implementation

--- app/scripts/formValidation.js
+++ app/scripts/formValidation.js
@@ -2,6 +2,7 @@ const contact = document.querySelector('#contact');
 const fullnameErr = document.querySelector('#fullname-err');
 const emailErr = document.querySelector('#email-err');
 const msgErr = document.querySelector('#msg-err');
+const successMsg = document.querySelector('#success-message');
 
 contact.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -11,7 +12,7 @@ contact.addEventListener('submit', (e) => {
   const message = elements[2].value;
   const fullnameCount = fullname.split(' ').length;
   if (fullnameCount < 2) {
-    fullnameErr.innerText = 'Please provide your full name!';
+    fullnameErr.innerText = 'Please provide your first and last name!';
   } else {
     fullnameErr.innerText = '';
   }
@@ -22,8 +23,36 @@ contact.addEventListener('submit', (e) => {
     emailErr.innerText = '';
   }
   if (message.trim().length === 0) {
-    msgErr.innerText = 'Please say something!';
+    msgErr.innerText = 'Please leave a message!';
   } else {
     msgErr.innerText = '';
+  }
+  if (fullnameCount >= 2 && email === lwEmail && message.trim().length > 0) {
+    const data = {
+      fullname,
+      email,
+      message,
+    };
+    fetch('https://formspree.io/f/xgebqege', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error('Response error!');
+    }).then((json) => {
+      if (json.ok) {
+        successMsg.classList.remove('invisible');
+        elements[0].value = '';
+        elements[1].value = '';
+        elements[2].value = '';
+      }
+    }).catch((error) => {
+      console.log(error);
+    });
   }
 });
